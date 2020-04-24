0a2f5a62cca15a202ded68f0852dbd5544471fde
Fishundergraduate <48595095+Fishundergraduate@users.noreply.github.com>
2020-04-24 23:47:43 +0900

    Add Description

--- scrabing.py
+++ scrabing.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 print("hello,world!")
 
 r = requests.get('https://weather.yahoo.co.jp/weather/jp/13/4410/13212.html')
+#r : html all source
 """
 with open('weatherdata.txt','w') as txtfile:
     print(r.headers,file=txtfile)
@@ -12,7 +13,7 @@ with open('weatherdata.txt','w') as txtfile:
     print(r.content,file=txtfile)
 """
 soup = BeautifulSoup(r.content,"html.parser")
-
+#soup : text analyze profile
 with open('weathertable.html','w') as tablefile:
     print(soup.find("div",id="yjw_pinpoint_today"),file=tablefile)
     print(soup.find("div",id = "yjw_pinpoint_tomorrow"),file=tablefile)
