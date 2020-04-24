c0e4a9336227f32145bd8f2d30eba219b0d4af5f
Fishundergraduate <48595095+Fishundergraduate@users.noreply.github.com>
2020-04-24 23:12:42 +0900

    webscra"p"ing したよ

--- scrabing.py
+++ scrabing.py
@@ -1,4 +1,18 @@
-import soup
 import requests
+from bs4 import BeautifulSoup
 
 print("hello,world!")
+
+r = requests.get('https://weather.yahoo.co.jp/weather/jp/13/4410/13212.html')
+"""
+with open('weatherdata.txt','w') as txtfile:
+    print(r.headers,file=txtfile)
+    print("------------------------")
+    print(r.encoding,file=txtfile)
+    print(r.content,file=txtfile)
+"""
+soup = BeautifulSoup(r.content,"html.parser")
+
+with open('weathertable.html','w') as tablefile:
+    print(soup.find("div",id="yjw_pinpoint_today"),file=tablefile)
+    print(soup.find("div",id = "yjw_pinpoint_tomorrow"),file=tablefile)
